3d3227e756fbf162a12ba6fe3890dcea7380fb09
monksandninjas <github email address>
2018-09-29 23:31:10 -0700

    added to sessions controller create action, set session and authenticated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+
+    return redirect_to login_path unless user
+
+    session[:user_id] = user.id
+
+    redirect_to root_url
   end
 end
