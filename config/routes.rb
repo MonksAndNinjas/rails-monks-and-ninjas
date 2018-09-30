08a8ffbbdac8434fcffdf1fa9bf6ec8f122334ea
monksandninjas <github email address>
2018-09-29 23:02:02 -0700

    changed users new and create route to read /signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static#home'
 
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
