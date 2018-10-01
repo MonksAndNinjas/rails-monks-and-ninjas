e4f71f9a15f1f32bd01eb57e2e041f57f7ec161e
monksandninjas <github email address>
2018-10-01 09:21:56 -0700

    added uid to users via migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_29_183450) do
+ActiveRecord::Schema.define(version: 2018_10_01_162019) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_09_29_183450) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
 end
