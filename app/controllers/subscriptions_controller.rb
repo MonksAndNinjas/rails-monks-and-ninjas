27273670e0886b003e0b3fda394836a8c4f403fc
monksandninjas <github email address>
2018-10-01 13:06:27 -0700

    added update code to subscriptions create action

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,4 +2,24 @@ class SubscriptionsController < ApplicationController
   def index
     @subscriptions = @current_user.subscriptions
   end
+
+  def new
+    @subscription = Subscription.new
+  end
+
+  def create
+    subscription = Subscription.new(subscription_params)
+
+    if subscription.save
+      redirect_to subscriptions_path
+    else
+      redirect_to new_subscription_path
+    end
+  end
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:name, :amount, :due_date)
+  end
 end
