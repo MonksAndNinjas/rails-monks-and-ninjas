b4d02a83e0740997d31a6a7a415613346867654d
monksandninjas <github email address>
2018-10-02 19:28:08 -0700

    added params method in priority items and added to create action

--- app/controllers/priority_items_controller.rb
+++ app/controllers/priority_items_controller.rb
@@ -1,12 +1,26 @@
 class PriorityItemsController < ApplicationController
 
   def index
-    @priority_items = PriorityItem.all
+    @priority_items = @current_user.priority_items
+    @priority_item = @current_user.priority_items.build
   end
 
   def create
+    @priority_item = @current_user.priority_items.build(priority_item_params)
+
+    if @priority_item.save
+      redirect_to priority_items_path
+    else
+      render :index
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def priority_items_params
+    params.require(:priority_item).permit(:title, :content)
   end
 end
