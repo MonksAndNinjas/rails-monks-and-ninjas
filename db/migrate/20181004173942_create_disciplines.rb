31aa68854b0d98568be1f158ecf5bfddd624775f
monksandninjas <github email address>
2018-10-04 10:40:21 -0700

    generated model for Disciplines and table as well

---
+++ db/migrate/20181004173942_create_disciplines.rb
@@ -0,0 +1,11 @@
+class CreateDisciplines < ActiveRecord::Migration[5.2]
+  def change
+    create_table :disciplines do |t|
+      t.string :title
+      t.string :content
+      t.string :duration
+
+      t.timestamps
+    end
+  end
+end
