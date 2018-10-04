24fa3579a75a7671ed30103a294a341548f8a9ae
monksandninjas <github email address>
2018-10-04 10:52:09 -0700

    added code to the action methods in disciplines controller

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -1,21 +1,49 @@
 class DisciplinesController < ApplicationController
 
   def index
-
+    @disciplines = @current_user.disciplines
   end
 
   def new
+    @discipline = @current_user.disciplines.build
   end
 
   def create
+    @discipline = @current_user.disciplines.build(discipline_params)
+
+    if @discipline.save
+      redirect_to discipline_path(@discipline)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @discipline = Discipline.find_by(id: params[:id])
   end
 
   def edit
+    @discipline = Discipline.find_by(id: params[:id]
   end
 
   def update
+    @discipline = Discipline.find_by_id(params[:id])
+
+    if @discipline.update(discipline_params)
+      redirect_to discipline_path(@discipline)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    Discipline.find(params[:id]).destroy
+    redirect_to disciplines_path
+  end
+
+  private
+
+  def discipline_params
+    params.require(:discipline).permit(:title, :content, :duration)
   end
 end
