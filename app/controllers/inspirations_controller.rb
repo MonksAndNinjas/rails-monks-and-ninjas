64cfe7a41173c4738fcd653bff829918f97f4fac
monksandninjas <github email address>
2018-10-03 20:18:50 -0700

    added all action routes for inspirations and added to index view for inspirations

--- app/controllers/inspirations_controller.rb
+++ app/controllers/inspirations_controller.rb
@@ -1,5 +1,24 @@
 class InspirationsController < ApplicationController
 
   def index
+    @inspirations = @current_user.inspirations
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
