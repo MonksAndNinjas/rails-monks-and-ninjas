fb5d0e11e177c6322db3a9789bd8f4e44c8f6a94
monksandninjas <blackbeltdiaries@gmail.com>
2019-02-06 20:49:14 +0000

    added belongs_to :user in subscriptionserializer.rb and has_many :subscriptions in userserializer.rb

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,3 +1,4 @@
 class SubscriptionSerializer < ActiveModel::Serializer
   attributes :id, :name, :amount, :due_day
+  belongs_to :user
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :birthdate, :email
+  has_many :subscriptions
 end
