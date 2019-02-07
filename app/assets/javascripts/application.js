d86508ba15327695bad861b922864a16fd913ebc
monksandninjas <blackbeltdiaries@gmail.com>
2019-02-07 20:24:46 +0000

    added jquery to application.js and added gem jquery-rails in gemfile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
+//= require jquery
+//= require jquery_ujs
 
 // Makes the button in Inspirations#index transparent, making only the words visible
 
