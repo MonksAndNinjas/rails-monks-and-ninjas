a3c879302718a124ef44bcdac7de8ae819b57cf2
monksandninjas <blackbeltdiaries@gmail.com>
2019-02-10 20:25:05 +0000

    created forms.js in javascripts folder for js code that constructs attr forms, I also moved getForm and getQuestForm function into it

---
+++ app/assets/javascripts/forms.js
@@ -0,0 +1,27 @@
+// Retrives form for long or short quest
+function getQuestForm (event, id) {
+  var data = $(event.target).data("id").split("-");
+  var attr = data[0];
+  var objectiveID = data[1];
+
+  $('.attr-actions').html('');
+  $('ul').html('');
+
+  $.ajax({
+    url: `/users/${id}/objectives/${objectiveID}/${attr}/new`,
+    dataType: 'script'
+  });
+}
+
+// Retrieves form for the attribute
+function getForm (event, id) {
+  var attr = $(event.target).data("id");
+
+    $('.attr-actions').html('');
+    $('ul').html('');
+
+  $.ajax({
+    url: `/users/${id}/${attr}/new`,
+    dataType: 'script'
+  });
+}
