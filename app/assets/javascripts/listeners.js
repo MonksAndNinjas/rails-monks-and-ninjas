47a062c06da6cec758e6c6be195c8631301a6164
monksandninjas <blackbeltdiaries@gmail.com>
2019-02-12 18:39:35 +0000

    added code in listeners.js and addFormListener function to display failed data in input

--- app/assets/javascripts/listeners.js
+++ app/assets/javascripts/listeners.js
@@ -25,9 +25,18 @@ function addFormListener () {
         url: `/users/${response.id}/${response.action}/new`,
         dataType: 'script',
         success: function () {
-          console.log(response.user_params);
+          var invalid_attr = response.user_params[`${response.action}_attributes`];
 
-          $('section').prepend(`<ul class="errors"><p>${response.messages.length} errors<br>Prohibited ${response.user} from being saved:</p></ul>`);
+          for (var key in invalid_attr){
+            var attr = `${response.action}_attributes`;
+            var search = `user[${attr}][${key}]`;
+            var value = invalid_attr[`${key}`];
+
+            $(`input[name="${search}"]`).val(value);
+          }
+
+
+          $('section').prepend(`<ul class="errors"><p>${response.messages.length} errors<br>Prohibited from being saved:</p></ul>`);
           for (let i = 0; i < response.messages.length; i++) {
             $('.errors').append(`<li>${response.messages[i]}</li>`);
           }
