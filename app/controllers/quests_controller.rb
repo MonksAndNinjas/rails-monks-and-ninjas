ee94e70c3b65388ab673698a1f320127632a033e
monksandninjas <blackbeltdiaries@gmail.com>
2019-02-23 00:06:43 +0000

    added function in quest_serializer to display created_at and added code in display_quest.html.erb to pass data to tell serializer to extend data

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,12 +1,13 @@
 class QuestsController < ApplicationController
 
   def quests_data
+    extended_data = params[:extended_data] || false
+
     quest = Quest.find(params[:id])
-    render json: quest, status: 200
+
+    render json: quest, scope: { extended_data: extended_data }, status: 200
   end
 
   def display_quest
-    @quest = Quest.find_by(id: params[:quest_id])
-    render "display_quest"
   end
 end
--- app/serializers/quest_serializer.rb
+++ app/serializers/quest_serializer.rb
@@ -1,3 +1,9 @@
 class QuestSerializer < ActiveModel::Serializer
-  attributes :description, :objective_id
+  attributes :description, :objective_id, :created_at
+
+  def attributes(arg1, arg2)
+      data = super
+      data.delete :created_at if !scope[:extended_data]
+      data
+  end
 end
