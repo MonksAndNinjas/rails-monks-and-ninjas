7341bfc8b658387a7cb320abd31de7195d8f1ead
monksandninjas <blackbeltdiaries@gmail.com>
2019-02-24 05:10:47 +0000

    added createdAt and updatedAt method in quest_serializer.rb to display readable format

--- app/serializers/quest_serializer.rb
+++ app/serializers/quest_serializer.rb
@@ -1,9 +1,22 @@
 class QuestSerializer < ActiveModel::Serializer
-  attributes :description, :objective_id, :created_at
+  attributes :description, :objective_id, :updatedAt, :createdAt
 
   def attributes(arg1, arg2)
-      data = super
-      data.delete :created_at if !scope[:extended_data]
-      data
+    data = super
+
+    if !scope[:extended_data]
+      data.delete :createdAt
+      data.delete :updatedAt
+    end
+
+    data
+  end
+
+  def createdAt
+    object.created_at.strftime("%m-%d-%Y")
+  end
+
+  def updatedAt
+    object.updated_at.strftime("%m-%d-%Y")
   end
 end
