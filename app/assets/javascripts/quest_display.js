f755ff2ad60a0fa858c8797e7d6070d68f85a3f2
monksandninjas <blackbeltdiaries@gmail.com>
2019-02-25 22:14:39 +0000

    added code to attachListener function in quest_display so as to increment idCount and get appropriate quest

--- app/assets/javascripts/quest_display.js
+++ app/assets/javascripts/quest_display.js
@@ -1,3 +1,5 @@
+var idCount = 0;
+
 function displayEditLink () {
   if (quest.objective_id === 1 && quest.count() > 21) {
     $('.actions').append(`<a href="/users/${$('.container').attr("data-id")}/` +
@@ -10,8 +12,43 @@ function displayEditLink () {
 
 function nextQuest () {
     $('.actions').append(`<button class="next-action">Next Quest</button>`);
+    attachListener();
 }
 
 function attachListener () {
-  $('.next-action').on("click", () => getNextQuest.apply(event));
+  $('.next-action').on("click", function () {
+    $('.actions').html('');
+    idCount++;
+    console.log(idCount);
+    getQuestData();
+  });
+}
+
+function getQuestData () {
+  var id = $('.container').attr("data-id");
+
+  $.getJSON("/users/" + id + "/user_data", { destination: 'quests' }, function(data) {
+    if (idCount > data.quests.length - 1) {
+      idCount = 0;
+    }
+
+    $.getJSON("/quests/" + data.quests[idCount].id + "/quests_data", { extended_data: 'true' }, function(quest_data) {
+      var attributes = {};
+
+      $.extend(attributes, data.quests[idCount], quest_data);
+
+      quest = new Quest(attributes);
+      console.log(quest, quest.objective(), quest.count(), quest.message());
+
+      displayEditLink();
+
+      nextQuest();
+
+      // next display items in proper place
+      // have funciton that gives the next quest
+
+      // need to have one page for displaying items and another for the quest javascript class -- .js
+      // split pages so not loading the pages in my_life
+    });
+  });
 }
